9a2d50ce50c351a8a1d79a0fe88a9ad3518d6907
liuhx20 <liuhx20@mails.tsinghua.edu.cn>
2025-02-20 08:44:20 +0800

    my_deepscaler

--- setup.py
+++ setup.py
@@ -12,7 +12,9 @@ setup(
         'pylatexenc',
         'sentence_transformers',
         'tabulate',
+        'setuptools',
         'flash_attn==2.7.3',
+        'wandb'
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
